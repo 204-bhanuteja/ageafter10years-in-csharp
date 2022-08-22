44a0e5ec0329f48ba2d7ecb579cbcb5799a717c9
204-bhanuteja <105474392+204-bhanuteja@users.noreply.github.com>
2022-08-22 22:19:57 +0530

    Add files via upload

---
+++ ageafter10years.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Input your current age:");
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Your age after 10 years:");
+        age = age + 10;
+        Console.Write("{0} ", age);
+    }
+}
